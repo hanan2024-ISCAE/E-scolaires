b260275c579e5a0ad7d87e90a8d9786d48a530bf
fatimetou <fatimetoum231@gmail.com>
2026-05-20 11:56:17 +0000

    "welcome"

--- e_scolaire_bakend/e_scolaire_bakend/settings.py
+++ e_scolaire_bakend/e_scolaire_bakend/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,5 +149,5 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-AUTH_USER_MODEL = 'api_PEI.Utilisateur'
-
+# AUTH_USER_MODEL = 'api_PEI.Utilisateur'
+AUTH_USER_MODEL = 'api.User'
